db95ce43038dd98c0be38c85e12b3bfd4e999a04
Sunti <sunti27@protonmail.com>
2021-06-18 11:21:02 +0200

    Update MainApp.java

--- MyTeam/src/pkgMain/MainApp.java
+++ MyTeam/src/pkgMain/MainApp.java
@@ -5,8 +5,12 @@ import pkgData.Player;
 public class MainApp {
 	
 	public static void main(String[] args) {
-		Player player = new Player(2, "Dragovic", "Defense");
+		Player player;
 		
+		player = new Player(2, "Dragovic", "Defense");
+		System.out.println("== " + player);
+		
+		player = new Player(4, "Hinteregger", "Defense");
 		System.out.println("== " + player);
 	}
 
